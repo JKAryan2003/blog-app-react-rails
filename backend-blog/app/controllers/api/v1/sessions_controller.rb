32e7e1f370c655c804fd48a62f8ab48292cf6636
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-04-08 14:39:36 +0530

    4: user creation in registrations controller

--- backend-blog/app/controllers/api/v1/sessions_controller.rb
+++ backend-blog/app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,17 @@
 module Api
   module V1
     class SessionsController < Api::V1::ApplicationController
+
+      def create
+        @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])   
+          token = JsonWebToken.encode({user_id: @user.id})
+          render json: {user: @user, token: token}
+        else
+          render json: {errors: "Invalid Credentials"}, status: :unauthorized
+        end
+      end
+
     end
   end
 end
