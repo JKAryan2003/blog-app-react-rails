166d6d5284a123b20f60e95eae40447c0199825b
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-04-11 07:10:25 +0530

    15: created small methods for authenticate_user

--- backend-blog/app/controllers/api/v1/posts_controller.rb
+++ backend-blog/app/controllers/api/v1/posts_controller.rb
@@ -3,13 +3,31 @@ module Api
 
     class PostsController < Api::V1::ApplicationController
 
+      include Authentication
+
       def index
+        @posts = Post.all
+        render json: {posts: @posts}, include: :user 
       end
       
       def create
+        current_user = find_current_user
+        @post = Post.new(post_params)
+        @post.user_id = current_user.id
+        
+        if @post.save
+          render json: {post: @post, message: "New Blog Created"}
+        else
+          render json: {errors: @post.errors.full_messages}
+        end
       end
 
-      
+      private
+
+      def post_params
+        params.require(:post).permit(:title, :content)
+      end
+
     end
 
   end
--- backend-blog/app/controllers/concerns/authentication.rb
+++ backend-blog/app/controllers/concerns/authentication.rb
@@ -2,45 +2,73 @@ module Authentication
   extend ActiveSupport::Concern
 
   def delete_allowed_token(header)
-    if header
-      token = header.split(' ').last
-      allowed_token = AllowList.find_by(token: token)
-      if allowed_token
-        allowed_token.destroy
-        render json: { message: 'Logout successfully' }, status: :ok
-      else
-        render json: { error: 'Token not found' }, status: :unauthorized
-      end
-    else
-      render json: { error: 'No token provided' }, status: :unauthorized
-    end
+    return render json: { error: 'No token provided' }, status: :unauthorized unless header
+    
+    # todo raise StandardError and rescue
+    token = header.split(' ').last
+    allowed_token = AllowList.find_by(token: token)
+    return render json: { error: 'Token not found' }, status: :unauthorized unless allowed_token
+   
+    allowed_token.destroy
+    render json: { message: 'Logout successfully' }, status: :ok
+    
   end
 
   def authenticate_user!
     header = request.headers['Authorization']
-    if header
-      token = header.split(' ').last
-      if token.nil?
-        return render json: { error: 'Unauthorized' }, status: :unauthorized
-      end
-
-      decoded_token = JsonWebToken.decode(token)
-      if decoded_token.nil?
-        return render json: { error: 'Unauthorized' }, status: :unauthorized
-      end
-      
-      @current_user = User.find_by(id: decoded_token[:user_id])
-      if @current_user.nil?
-        return render json: { error: 'User not found' }, status: :unauthorized
-      end
-
-      allow_list_entry = AllowList.find_by(token: token)
-      if allow_list_entry.nil? || allow_list_entry.expires_at < Time.current
-        render json: { error: 'Token has expired' }, status: :unauthorized
-      end
-    else
-      return render json: { error: 'You need to log in first' }, status: :unauthorized
+    return render json: { error: 'You need to log in first' }, status: :unauthorized unless header
+    
+    token = header.split(' ').last
+    if token.nil?
+      return render json: { error: 'Unauthorized' }, status: :unauthorized
     end
 
+    decoded_token = JsonWebToken.decode(token)
+    if decoded_token.nil?
+      return render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+    
+    
+    @current_user = User.find_by(id: decoded_token[:user_id])
+    if @current_user.nil?
+      return render json: { error: 'User not found' }, status: :unauthorized
+    end
+
+    allow_list_entry = AllowList.find_by(token: token)
+    if allow_list_entry.nil? || allow_list_entry.expires_at < Time.current
+      render json: { error: 'Token has expired' }, status: :unauthorized
+    end
+   
   end
+
+  def find_header
+    header = request.headers['Authorization']
+    return render json: { error: 'You need to log in first' }, status: :unauthorized unless header
+    header
+  end  
+
+  def decode_token
+    header = find_header
+    
+    token = header.split(' ').last
+    if token.nil?
+      return render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+
+    decoded_token = JsonWebToken.decode(token)
+    if decoded_token.nil?
+      return render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+    decoded_token
+  end
+
+  def find_current_user
+    decoded_token = decode_token
+    @current_user = User.find_by(id: decoded_token[:user_id])
+    if @current_user.nil?
+      return render json: { error: 'User not found' }, status: :unauthorized
+    end
+    @current_user
+  end
+
 end
