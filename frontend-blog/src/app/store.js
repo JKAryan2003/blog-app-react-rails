0b53068bbbdbe65e577ca578fcc6778d4ff42b59
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-04-14 06:57:24 +0530

    20: created commentSlice

--- frontend-blog/src/app/store.js
+++ frontend-blog/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postReducer from "../features/post/postSlice"
+import commentReducer from "../features/comment/commentSlice"
 
 const store = configureStore({
   reducer: {
-    post: postReducer
+    post: postReducer,
+    comment: commentReducer,
   }
 })
 
---
+++ frontend-blog/src/features/comment/commentSlice.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  loading: false,
+  comments: [],
+  comment: "",
+}
+
+export const fetchComments = createAsyncThunk('comment/fetchComments', async () => {
+  const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/comments`)
+  return response.data;
+})
+
+export const addComment = createAsyncThunk('comment/addComment', async (input) => {
+  const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/comments`, input)
+  return response.data;
+})
+
+const commentSlice = createSlice({
+  name: 'comment',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchComments.fulfilled, (state, action) => {
+      state.comments = action.payload
+    }),
+    builder.addCase(addComment.fulfilled, (state, action) => {
+      state.comment = action.payload
+    })
+  }
+})
+
+export default commentSlice.reducer
